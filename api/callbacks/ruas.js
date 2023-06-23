bcf577a44a8dfb3e218ae943d041e0a469078981
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2023-06-23 16:43:43 +0100

    Create ruas.js

---
+++ api/callbacks/ruas.js
@@ -0,0 +1,31 @@
+// Callbacks for the ruas route.
+
+/**
+ * Module dependencies
+ */
+const Controller = require('../controllers/controller')
+const { RuaController } = require('../controllers/rua')
+
+/**
+ * Controller for the Rua model
+ * @extends Controller
+ * @type {RuaController}
+ */
+const controller = new RuaController()
+
+/**
+ * Callback for the index / route.
+ * @details This is the callback for the index route. It simply returns a JSON object with a message.
+ * @param {*} req - Express request object.
+ * @param {*} res - Express response object.
+ * @param {*} next - Express next middleware function.
+ */
+exports.index = async (req, res, next) => {
+    try{
+        const documentArray = await controller.getAllSortedByName(Controller.SORT.ASC)
+        res.status(200).jsonp({ ruas: documentArray })
+    } catch (err) {
+        console.error(err.stack)
+        res.status(500).jsonp({ error: err.message })
+    }
+}
