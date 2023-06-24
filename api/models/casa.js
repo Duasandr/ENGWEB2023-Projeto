2ec2e50924b4546197f21167ddf42495ba2e2540
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2023-06-24 17:48:47 +0100

    Create casa.js
    Created casa model

---
+++ api/models/casa.js
@@ -0,0 +1,27 @@
+/**
+ * Model dependencies
+ */
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+/**
+ * Casa model definition
+ * @type {Schema}
+ * @property {String} _id 
+ * @property {String} numero
+ * @property {String} foro
+ * @property {String[]} enfiteuta - Array of _id of Enfiteuta objects
+ * @property {String[]} descricao
+ * @property {String} vista
+ */
+const CasaSchema = new Schema({
+    _id: String,
+    "numero": { type: String, required: true },
+    "foro": String,
+    "enfiteuta": [String],  // Array of _id
+    "descricao": [String],
+    "vista": String
+})
+
+// Export model with name, schema and collection
+module.exports = mongoose.model('Casa', CasaSchema, 'Casas')
