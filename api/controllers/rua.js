911d4d1af1f3b0883efe0cc7eef0009cd24f4c5a
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2023-06-24 17:48:38 +0100

    Update rua.js
    Added default parameter value for getAllSortedByName

--- api/controllers/rua.js
+++ api/controllers/rua.js
@@ -18,10 +18,12 @@ class RuaController extends Controller {
 
     /**
      * Finds all documents ordered by name.
+     * @details The order is ascending by default, but can be changed to descending by passing -1 
+     * as the order parameter or ```RuaController.SORT.DESC``` for readability.
      * @param {*} order Represents the order (1 for ascending, -1 for descending)
-     * @returns {Promise} - The promise of the query
+     * @returns {Promise} The promise of the query
      */
-    getAllSortedByName(order) { return this.getAllSorted({ "nome": order }) }
+    getAllSortedByName(order = Controller.SORT.ASC) { return this.getAllSorted({ "nome": order }) }
 }
 
 module.exports = { RuaController }
