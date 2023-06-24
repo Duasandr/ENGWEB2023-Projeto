a599d5aac24ffdb8aa2e4b958f44cfe96db44c45
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2023-06-24 16:17:21 +0100

    Create imagem.js
    Created callbacks for api/imagens route

---
+++ api/callbacks/imagem.js
@@ -0,0 +1,31 @@
+// Callbacks for the imagem route.
+
+/**
+ * Module dependencies
+ */
+const { ImagemController } = require('../controllers/imagem')
+
+/**
+ * Controller for the Imagem model
+ * @extends Controller
+ * @type {ImagemController}
+ */
+const controller = new ImagemController()
+
+/**
+ * Callback for the index api/imagens route.
+ * @details Writes a JSON object with all the imagens in the database to the response. 
+ * The JSON object is stored in the imagens property, with its value being an array of Imagem objects.
+ * @param {*} req - Express request object.
+ * @param {*} res - Express response object.
+ * @param {*} next - Express next middleware function.
+ */
+exports.index = async (req, res, next) => {
+    try{
+        const documentArray = await controller.getAll()
+        res.status(200).jsonp({ imagens: documentArray })
+    } catch (err) {
+        console.error(err.stack)
+        res.status(500).jsonp({ error: err.message })
+    }
+}
