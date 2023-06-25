033b4029675660cb3418a5ba78f204222529ecbc
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2023-06-25 18:44:39 +0100

    Update imagem.js
    Added idReferencia as a foreign key.

--- api/models/imagem.js
+++ api/models/imagem.js
@@ -8,11 +8,14 @@ const Schema = mongoose.Schema
  * Imagem model definition
  * @type {Schema}
  * @property {String} _id
+ * @property {String} referencia - Street reference. 
+ * The name is ambigous because an image can be referenced by multiple entities. For sake of simplicity, I chose to use the Rua entity only.
  * @property {String} path - Path to image
  * @property {String} legenda - Image caption
  */
 const ImagemSchema = new Schema({
     _id: String,
+    "idReferencia": { type: String, required: true, ref: 'Rua' },
     "path": { type: String, required: true },
     "legenda": String
 })
