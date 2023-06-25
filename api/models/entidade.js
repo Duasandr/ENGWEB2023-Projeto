2f438deff07e3346186f6480f8cd8c87cd1f9a67
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2023-06-25 18:47:29 +0100

    Update entidade.js
    Removed tipo as it was created a new entity that represents the type.

--- api/models/entidade.js
+++ api/models/entidade.js
@@ -8,12 +8,10 @@ const Schema = mongoose.Schema
  * Entidade model definition
  * @type {Schema}
  * @property {String} _id
- * @property {String} tipo - pessoa, instituição, empresa, família
  * @property {String} nome
  */
 const EntidadeSchema = new Schema({
     _id: String,
-    "tipo": { type: String, required: true, enum: ['pessoa', 'instituição', 'empresa', 'família'] },
     "nome": { type: String, required: true }
 })
 
